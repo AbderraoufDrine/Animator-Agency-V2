8e02820211f0c402cdff967254cce41b5765f9d9
Abderraouf <abderraoufdrine@gmail.com>
2024-01-04 18:57:12 -0500

    Changed main blog page

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,19 +1,20 @@
 import BlogCard from "@/components/blogCard/blogCard";
+import { getPosts } from "@/lib/action";
 import React from "react";
 
-const getData = async () => {
-  const res = await fetch("https://animator-agency-v2.vercel.app/api/blog", {
-    cache: "no-store",
-  });
+// const getData = async () => {
+//   const res = await fetch("https://animator-agency-v2.vercel.app/api/blog", {
+//     cache: "no-store",
+//   });
 
-  if (!res.ok) {
-    throw new Error("Wrong fetch");
-  }
+//   if (!res.ok) {
+//     throw new Error("Wrong fetch");
+//   }
 
-  return res.json();
-};
+//   return res.json();
+// };
 const Blog = async () => {
-  const posts = await getData();
+  const posts = await getPosts();
   return (
     <div className="flex flex-wrap gap-5">
       {posts.map((post) => (
