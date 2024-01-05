6b58bd5a1b1728631f3e842f297b1d9c949fac38
Abderraouf <abderraoufdrine@gmail.com>
2024-01-04 19:52:42 -0500

    Added Logo

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import logo from "../../../public/animator-agency logo.png";
+import Image from "next/image";
 
 const Footer = () => {
   return (
     <div className="flex flex-col md:flex md:justify-between md:flex-row items-center h-[75px] mt-5 text-gray-500">
-      <div className="font-bold">My Logo</div>
+      <div className="font-bold">
+        {" "}
+        <Image src={logo} alt="logo" width={40} height={40} />
+      </div>
       <div className="text-md">Animator Agency V2 Ⓒ</div>
     </div>
   );
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Links from "./links/Links";
 import { auth } from "@/lib/auth";
+import logo from "../../../public/animator-agency logo.png";
+import Image from "next/image";
 
 const Navbar = async () => {
   const session = await auth();
 
   return (
     <div className="h-[100px] flex items-center justify-between">
-      <div className="text-3xl font-bold">Logo</div>
+      <div className="text-3xl font-bold">
+        <Image src={logo} alt="logo" width={75} height={75} />
+      </div>
       <div>
         <Links session={session} />
       </div>
