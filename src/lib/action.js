a25388c988b70b9d79902e8447c8203d269b2c66
Abderraouf <abderraoufdrine@gmail.com>
2024-01-04 19:03:18 -0500

    Added Security

--- src/lib/action.js
+++ src/lib/action.js
@@ -47,10 +47,12 @@ export const addUser = async (previousState, formData) => {
 
   try {
     connect();
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
     const newUser = new User({
       username,
       email,
-      password,
+      password: hashedPassword,
       img,
     });
 
