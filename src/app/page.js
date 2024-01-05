f2a5366db176a802d87d3d865bbd5570ad7df99c
Abderraouf <abderraoufdrine@gmail.com>
2024-01-04 19:25:33 -0500

    Added Extra Path From Buttons

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import React from "react";
 import hero from "../../public/hero.gif";
+import Link from "next/link";
 
 const Home = () => {
   return (
@@ -10,17 +11,23 @@ const Home = () => {
           Challenges in creative age
         </h1>
         <p className="text-lg">
-          We beleive that desigining products and services in close partnership
+          We believe that designing products and services in close partnership
           with our clients is the only way to have a real impact on their
           business.
         </p>
         <div className="flex gap-5 items-center justify-center md:justify-normal">
-          <button className=" p-5 min-w-28 cursor-pointer rounded-md border-none bg-btnBg text-white">
+          <Link
+            href="/about"
+            className=" p-5 min-w-28 cursor-pointer rounded-md border-none bg-btnBg text-white"
+          >
             Learn More
-          </button>
-          <button className=" p-5 min-w-28 cursor-pointer rounded-md border-none bg-white text-black">
+          </Link>
+          <Link
+            href="/contact"
+            className=" p-5 min-w-28 cursor-pointer rounded-md border-none bg-white text-black"
+          >
             Contact
-          </button>
+          </Link>
         </div>
       </div>
       <div className="relative flex-1 hidden xl:block">
